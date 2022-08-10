005fd42355e6db960d000ea0aa824fd1f5111f57
Selin Ayyildiz <xcadyix@gmail.com>
2022-08-09 20:06:11 -0400

    commit tree c087e9c98e77f0507db0903e227636ca705a841a

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//function capitalizeEach(sen) {
+//  var array1 = sen.split('')
+ // var newarray1 = []
+  //for(var x = 0; x < array1.length; x++){
+    //newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+//}
+//return newarray1.join(' ') 
+//}
+
+function capitalizeEach(str) {
+  var array1 = str.split(' ');
+  var newarray1 = [];
+    
+  for(var x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
 }
+
+  function titleCased() {
+     return tutorials.map(capitalizeEach)
+  }
